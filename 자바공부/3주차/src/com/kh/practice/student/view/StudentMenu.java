682c76450a2125e2edb9683994b6849ec25809cc
hongsuk2615 <hongsuk2615@gmail.com>
2022-11-23 14:49:18 +0900

    java

--- 자바공부/3주차/src/com/kh/practice/student/view/StudentMenu.java
+++ 자바공부/3주차/src/com/kh/practice/student/view/StudentMenu.java
@@ -16,7 +16,7 @@ public class StudentMenu {
 		System.out.println();
 		
 		System.out.println("========== 학생 성적 출력 ==========");
-		System.out.println("학생 점수 합계 : " + ssm.avgScore()[0]);
+		System.out.println("학생 점수 합계 : " + (int)ssm.avgScore()[0]);
 		System.out.println("학생 점수 평균 : " + ssm.avgScore()[1]);
 		System.out.println();
 		
@@ -25,7 +25,7 @@ public class StudentMenu {
 			if (ssm.printStudent()[i].getScore() < StudentController.CUT_LINE) {
 				System.out.println(ssm.printStudent()[i].getName() + "학생은 재시험 대상입니다.");
 			} else {
-				System.out.println(ssm.printStudent()[i].getName() + "학생은 통과 입니다.");
+				System.out.println(ssm.printStudent()[i].getName() + "학생은 통과입니다.");
 			}
 		}
 	}
