c16dd2b4251a391c0b6496d57e76829c0061fc67
hongsuk2615 <hongsuk2615@gmail.com>
2022-11-28 16:50:35 +0900

    java

--- 자바공부/4주차/Homework/09_Polymorphism_Homework_김홍석/src/practice3/controller/AnimalManager.java
+++ 자바공부/4주차/Homework/09_Polymorphism_Homework_김홍석/src/practice3/controller/AnimalManager.java
@@ -1,6 +1,8 @@
 package practice3.controller;
 
 import practice3.model.vo.Animal;
+import practice3.model.vo.Cat;
+import practice3.model.vo.Dog;
 
 public class AnimalManager {
 
@@ -9,10 +11,17 @@ public class AnimalManager {
 		Animal[] animal = new Animal[5];
 		for (int i = 0; i < animal.length; i++) {
 			if (Math.random()<0.5) {
-				animal[i] = new Cat
+				animal[i] = new Cat("냐옹이"+i, "고양이", "한국", "검정");
+			}else {
+				animal[i] = new Dog("멍멍이"+i, "강아지", (int)(4+5*Math.random()));
 			}
 
 		}
+		
+		for(Animal anim : animal) {
+			anim.speak();
+		}
 	}
+	
 
 }
