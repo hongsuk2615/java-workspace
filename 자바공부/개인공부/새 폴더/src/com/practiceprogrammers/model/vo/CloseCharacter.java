26cd7756926d5df1dda46e38e11031f4ad7c0b03
hongsuk2615 <hongsuk2615@gmail.com>
2022-12-18 21:54:58 +0900

    java

--- 자바공부/개인공부/새 폴더/src/com/practiceprogrammers/model/vo/CloseCharacter.java
+++ 자바공부/개인공부/새 폴더/src/com/practiceprogrammers/model/vo/CloseCharacter.java
@@ -3,6 +3,9 @@ package com.practiceprogrammers.model.vo;
 public class CloseCharacter {
 	public int[] solution(String s) {
 		int[] answer = new int[s.length()];
+		if (s.length() != 0) {
+			answer[0] = -1;
+		}
 		for (int index = 0; index < s.length(); index++) {
 			String str = s.substring(0, index + 1);
 			for (int i = index - 1; i >= 0; i--) {
