b892d434b77e31cfce124d410d22ce3df087b7a2
hongsuk2615 <hongsuk2615@gmail.com>
2023-01-23 22:49:17 +0900

    java

---
+++ 자바공부/개인공부/새 폴더/src/com/practiceprogrammers/model/vo/과일장수.java
@@ -0,0 +1,9 @@
+package com.practiceprogrammers.model.vo;
+
+public class 과일장수 {
+	public int solution(int k, int m, int[] score) { // 팔수있는 상자수 : score.length / m
+        int answer = 0;
+        
+        return answer;
+    }
+}
